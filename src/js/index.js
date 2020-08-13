a1753da6d27ee97e8cac030973cd30836e5df71b
Virus <vladprodan6@gmail.com>
2020-08-14 00:05:40 +0300

    history, styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,31 @@
 import 'bootstrap';
+// eslint-disable-next-line no-unused-vars
+import Swiper, { Navigation, Pagination } from 'swiper';
+import '../scss/swiper/swiper-bundle.scss';
+import '../js/swiper-bundle.min.js';
 
-import '../scss/index.scss';
-
-$('#alert').on('click', () => {
-  alert('jQuery works!');
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
-// Your jQuery code
+$('#nav-icon3').click(function() {
+  $(this).toggleClass('open')
+  $('#overlay').toggleClass('open');
+  $('.intro').toggleClass('active');
+});
+
+//  $(document).ready(function(){
+// 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+// 		$(this).toggleClass('open');
+// 	});
+// });
+
+import '../scss/index.scss';
